00242c66b270191b230708d2d8affc46c314a3d5
akhtareijaz <akhtareijaz@gmail.com>
2026-03-09 16:24:44 +0700

    [RED] Add tests for PaymentRepository

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepositoryTest.java
@@ -0,0 +1,93 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentRepositoryTest {
+    private PaymentRepository paymentRepository;
+    private List<Payment> payments;
+
+    @BeforeEach
+    void setUp() {
+        paymentRepository = new PaymentRepository();
+        payments = new ArrayList<>();
+
+        Map<String, String> paymentData1 = new HashMap<>();
+        paymentData1.put("voucherCode", "ESHOP1234ABC5678");
+        Payment payment1 = new Payment("f8dd3b5d-2b4d-4f83-a4e6-a1a77d778f9d", PaymentMethod.VOUCHER.getValue(), "SUCCESS", paymentData1);
+
+        Map<String, String> paymentData2 = new HashMap<>();
+        paymentData2.put("address", "Jl. Cisokan No. 225");
+        paymentData2.put("deliveryFee", "10000");
+        Payment payment2 = new Payment("bf48e85a-7b44-484d-9f84-95e1cb8b7f20", PaymentMethod.COD.getValue(), "REJECTED", paymentData2);
+
+        payments.add(payment1);
+        payments.add(payment2);
+    }
+
+    @Test
+    void testSaveCreate() {
+        Payment payment = payments.get(0);
+        Payment result = paymentRepository.save(payment);
+
+        Payment findResult = paymentRepository.findById(payment.getId());
+        assertEquals(payment.getId(), result.getId());
+        assertEquals(payment.getId(), findResult.getId());
+        assertEquals(payment.getMethod(), findResult.getMethod());
+        assertEquals(payment.getStatus(), findResult.getStatus());
+        assertSame(payment.getPaymentData(), findResult.getPaymentData());
+    }
+
+    @Test
+    void testSaveUpdate() {
+        Payment payment = payments.get(1);
+        paymentRepository.save(payment);
+
+        Payment newPayment = new Payment(payment.getId(), payment.getMethod(), PaymentStatus.REJECTED.getValue(), payment.getPaymentData());
+        Payment result = paymentRepository.save(newPayment);
+
+        Payment findResult = paymentRepository.findById(payment.getId());
+        assertEquals(payment.getId(), result.getId());
+        assertEquals(payment.getId(), findResult.getId());
+        assertEquals(PaymentStatus.REJECTED.getValue(), findResult.getStatus());
+    }
+
+    @Test
+    void testFindByIdIfFound() {
+        for (Payment payment : payments) {
+            paymentRepository.save(payment);
+        }
+
+        Payment findResult = paymentRepository.findById(payments.get(1).getId());
+        assertEquals(payments.get(1).getId(), findResult.getId());
+        assertEquals(payments.get(1).getMethod(), findResult.getMethod());
+    }
+
+    @Test
+    void testFindByIdIfNotFound() {
+        for (Payment payment : payments) {
+            paymentRepository.save(payment);
+        }
+
+        Payment findResult = paymentRepository.findById("bf48e85a-7b44-484d-9f84-95e1cb8b7f21");
+        assertNull(findResult);
+    }
+
+    @Test
+    void testGetAllPayments() {
+        for (Payment payment : payments) {
+            paymentRepository.save(payment);
+        }
+
+        List<Payment> paymentList = paymentRepository.getAllPayments();
+        assertEquals(2, paymentList.size());
+    }
+}
