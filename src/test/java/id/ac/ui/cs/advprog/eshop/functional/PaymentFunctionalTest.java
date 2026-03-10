788dd3d08791b89ce99ca13c4f4ac64bfd8ca9e0
akhtareijaz <akhtareijaz@gmail.com>
2026-03-10 18:56:43 +0700

    [RED] Implement functional tests for Payment features.

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/PaymentFunctionalTest.java
@@ -0,0 +1,47 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class PaymentFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void paymentDetailPage_isAccessible(ChromeDriver driver) throws Exception {
+        driver.get(baseUrl + "/payment/detail");
+
+        String pageTitle = driver.getTitle();
+        assertEquals("Payment Detail", pageTitle);
+    }
+
+    @Test
+    void paymentAdminListPage_isAccessible(ChromeDriver driver) throws Exception {
+        driver.get(baseUrl + "/payment/admin/list");
+
+        String pageTitle = driver.getTitle();
+        assertEquals("All Payments", pageTitle);
+    }
+}
